2e55f340ba02d4ef9eec040d1771c72d9755fec5
JorgeLuisRodriguezCruz <rodriguezcruz088@gmail.com>
2024-03-23 19:54:25 -0600

    Se comprueba la conexion a la base de datos y se implementa la carga de datos

--- Project BD1 Progra2/src/config.js
+++ Project BD1 Progra2/src/config.js
@@ -2,6 +2,4 @@ require('dotenv').config();
 
 const port = process.env.PORT || 4000
 
-console.log("AAAqui: "+process.env.PORT);
-
 module.exports = port;
--- Project BD1 Progra2/src/controllers/user.controller.js
+++ Project BD1 Progra2/src/controllers/user.controller.js
@@ -1,7 +1,43 @@
 const { getConnection } = require("../database/connection");
+const path = require("path");
 
+/*
+    Ademas de cargar la vista "login" obtiene la direccion completa del archivo.xml
+    y se la envia a un SP para que con esta obtenga los datos del archivo y los procese
+*/
 const loging = async (req, res) => {
-    res.render('login')
-}
+  try {
+    // Ruta relativa del archivo que quieres obtener
+    const rutaRelativa = "../datos.xml";
+    // Unir la ruta relativa con la carpeta actual (__dirname)
+    const rutaCompleta = path.join(__dirname, rutaRelativa);
 
-module.exports = {loging}
+    console.log("Esta es la ruta:", rutaCompleta);
+
+    // Obtener una conexión desde el pool de conexiones
+    const pool = await getConnection();
+
+    // Llamada al procedimiento almacenado loadXMLData
+    const result = await pool
+      .request()
+      .input("xmlFilePath", rutaCompleta)
+      .output("OutResulTCode", 0)
+      .execute("loadXMLData");
+
+    if (result.output.OutResulTCode == 0) {
+      console.log("Se cargaron los datos :D");
+    } else {
+      console.log("Algo salio mal con la carga de datos :O");
+    }
+
+    // Cerrar la conexión al pool
+    pool.close();
+  } catch (error) {
+    // Manejar errores internos del servidor
+    console.log(error.message);
+  }
+  //Carga la vista login
+  res.render("login");
+};
+
+module.exports = { loging };
