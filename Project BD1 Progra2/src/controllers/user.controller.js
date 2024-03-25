e03563648d5709182be68bea788c54b7dba5db14
JorgeLuisRodriguezCruz <rodriguezcruz088@gmail.com>
2024-03-24 22:49:24 -0600

    Versión final de estructura básica del proyecto

--- Project BD1 Progra2/src/controllers/user.controller.js
+++ Project BD1 Progra2/src/controllers/user.controller.js
@@ -1,43 +1,43 @@
 const { getConnection } = require("../database/connection");
 const path = require("path");
 
-/*
-    Ademas de cargar la vista "login" obtiene la direccion completa del archivo.xml
-    y se la envia a un SP para que con esta obtenga los datos del archivo y los procese
-*/
-const loging = async (req, res) => {
+//Se la envia la direccion completa del archivo XML a un SP para que con 
+//esta obtenga los datos del archivo y los procese
+const root = async (req, res) => {
   try {
     // Ruta relativa del archivo que quieres obtener
-    const rutaRelativa = "../datos.xml";
+    const relativeRoute = "../datos.xml";
     // Unir la ruta relativa con la carpeta actual (__dirname)
-    const rutaCompleta = path.join(__dirname, rutaRelativa);
-
-    console.log("Esta es la ruta:", rutaCompleta);
-
+    const completeRoute = path.join(__dirname, relativeRoute);
+  
     // Obtener una conexión desde el pool de conexiones
     const pool = await getConnection();
-
+  
     // Llamada al procedimiento almacenado loadXMLData
     const result = await pool
       .request()
-      .input("xmlFilePath", rutaCompleta)
+      .input("xmlFilePath", completeRoute)
       .output("OutResulTCode", 0)
       .execute("loadXMLData");
-
+  
     if (result.output.OutResulTCode == 0) {
       console.log("Se cargaron los datos :D");
     } else {
       console.log("Algo salio mal con la carga de datos :O");
     }
-
     // Cerrar la conexión al pool
     pool.close();
   } catch (error) {
     // Manejar errores internos del servidor
     console.log(error.message);
   }
+  res.redirect("login");
+}
+
+//Cargar la vista "login"
+const loging = (req, res) => {
   //Carga la vista login
   res.render("login");
 };
 
-module.exports = { loging };
+module.exports = { root, loging };
--- Project BD1 Progra2/src/routes/user.routes.js
+++ Project BD1 Progra2/src/routes/user.routes.js
@@ -1,10 +1,12 @@
 // Importación del módulo 'express' para la creación de rutas (URLs)
 const Router = require('express');
 // Importación de funciones desde el controlador employe.controller
-const { loging } = require('../controllers/user.controller');
+const { root, loging } = require('../controllers/user.controller');
 
 const userRouter = Router();
 
-userRouter.get('/', loging)
+userRouter.get('/', root)
+
+userRouter.get('/login', loging)
 
 module.exports = { userRouter };
